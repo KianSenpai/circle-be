aa1a5659ce14b58c5acd269a34264e312129c29e
Kian AliAkbari <kiana@kaytu.io>
2024-03-20 03:18:50 +0400

    feat: added netlify

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,8 +1,8 @@
 import express, { Router } from 'express'
 import swaggerUi from 'swagger-ui-express'
-import swaggerDocument from '../src/docs/swagger.json'
-import catchAsyncErrors from '../src/utils/catchAsyncErrors'
-import { getAllBooks, getUniqueBook, purchaseBook } from '../src/controller/booksController'
+import swaggerDocument from '../../src/docs/swagger.json'
+import catchAsyncErrors from '../../src/utils/catchAsyncErrors'
+import { getAllBooks, getUniqueBook, purchaseBook } from '../../src/controller/booksController'
 import serverless from "serverless-http"
 
 const api = express()
@@ -15,6 +15,6 @@ router.get('/', catchAsyncErrors(getAllBooks))
 router.get('/:id', catchAsyncErrors(getUniqueBook))
 router.post('/:id/purchase', catchAsyncErrors(purchaseBook))
 
-api.use('/.netlify/functions/', router)
+api.use('/books', router)
 
 export const handler = serverless(api)
