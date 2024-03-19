c8456db6eadd0f42cc08e5409b8402b93884b130
Kian AliAkbari <kiana@kaytu.io>
2024-03-19 05:35:24 +0400

    feat: added netlify

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import booksRouter from './router/booksRouter'
 import { errorHandler } from './middleware/errorHandler'
 import ErrorHandler from './utils/ErrorHandler'
 import cors from 'cors'
+import serverless from "serverless-http"
 
 const app = express()
 const port = 8000
@@ -10,13 +11,15 @@ const port = 8000
 app.use(express.json())
 app.use(cors())
 
-app.use('/books', booksRouter)
+app.use('/.netlify/functions/books', booksRouter)
 
 app.use((_req, _res, next) => {
     next(new ErrorHandler('Route not found', 404))
 })
 
 app.use(errorHandler)
+
+export const handler = serverless(app)
 
 app.listen(port, () => {
     console.log(`🚀 Example app listening at http://localhost:${port}`)
